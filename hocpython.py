600941ff293ea218c49434bb3450f43538610ad4
Hòa Vũ <qhoa83@gmail.com>
2021-08-30 23:44:01 +0700

    Initial

---
+++ hocpython.py
@@ -0,0 +1,12 @@
+# Toi hoc python
+print("Toi hoc python, python case sensitive")
+print("Ngon ngu lap trinh nhay cam qua")
+
+# Dat bien nhu an keo
+a = 3
+b = 4
+print(a+b)
+print(a*b)
+print(a/b)
+c = a + b
+print(c)
---
+++ portcheck.py
@@ -0,0 +1,22 @@
+import socket
+import time
+from datetime import datetime
+
+host = input("IP Address: ")
+port = input("Port: ")
+count = input("Count: ")
+server = ((host), int(port))
+print(server)
+n = int(count)
+while n > 0:
+    n -= 1
+    time.sleep(2)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex(server)
+    if result == 0:
+        print(n,".","Port is open", server, "at", datetime.now().time())
+    else:
+        print("Port is close", server, "at", datetime.now().time())
+    sock.close()
+else:
+    print("End !!!!!")
